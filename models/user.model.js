a200a020aaa45b6834b25877ab0964df6041cb0f
Safi Hanbal <safihanbal@gmail.com>
2022-09-27 17:37:40 +0530

    Updated image src in dev-data

--- models/user.model.js
+++ models/user.model.js
@@ -78,13 +78,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
-//   this.password = await bcrypt.hash(this.password, 12);
-//   this.passwordConfirm = undefined;
-//   next();
-// });
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
 
 userSchema.pre('save', function (next) {
   if (!this.isModified || this.isNew) return next();
